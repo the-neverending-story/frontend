bfadca167d366bd978e682279f7e1efbdabf7cb4
Brandon Loveall <bloveall02@gmail.com>
2025-10-07 14:51:00 -0600

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { App } from './app/app';
+import { provideRouter } from '@angular/router';
+
+bootstrapApplication(App, {
+  providers: [
+    provideRouter([])
+  ]
+})
+  .catch((err) => console.error(err));
