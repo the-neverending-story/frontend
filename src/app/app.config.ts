aaf0ca6bdbd6f8dea4abcb48dbc898c91e18312f
Brandon Loveall <bloveall02@gmail.com>
2026-03-25 12:42:14 -0600

    apollo and graphql added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideApollo } from 'apollo-angular';
+import { InMemoryCache } from '@apollo/client';
+import { HttpLink } from 'apollo-angular/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideHttpClient(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideApollo(() => {
+      const httpLink = inject(HttpLink);
+      return {
+        link: httpLink.create({ uri: 'http://localhost:3000/graphql' }),
+        cache: new InMemoryCache(),
+      };
+    }),
   ]
 };
--- src/app/components/unique/main-menu/main-menu.ts
+++ src/app/components/unique/main-menu/main-menu.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-main-menu',
   imports: [],
+  providers: [Apollo],
   templateUrl: './main-menu.html',
   styleUrl: './main-menu.scss'
 })
-export class MainMenu {
+export class MainMenu implements OnInit {
+  constructor(private readonly apollo: Apollo) {}
 
+  ngOnInit() {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            user(id: 1) {
+              id
+              username
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe((result: any) => {
+        console.log(result);
+      });
+  }
 }
