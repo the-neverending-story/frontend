8ea2af055298263a30619af0425a52017b03cf2b
Brandon Loveall <bloveall02@gmail.com>
2026-04-03 14:24:24 -0600

    correct backend uri added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,7 +16,7 @@ export const appConfig: ApplicationConfig = {
 
       return {
         link: httpLink.create({
-          uri: 'http://172.31.49.106:3000',
+          uri: 'http://backend-alb-1352487503.us-west-2.elb.amazonaws.com:3000',
           withCredentials: true
         }),
         cache: new InMemoryCache(),
